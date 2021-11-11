e705147328643216fbaf30d6504859dccd7f3d24
Elio Lu <1520622848@qq.com>
2021-11-11 23:03:09 +0800

    fix:

--- src/pages/Comments.js
+++ src/pages/Comments.js
@@ -1,19 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Table, Space, Pagination, Modal } from 'antd'
 
-function confirm(record) {
-    Modal.confirm({
-        title: 'Confirm',
-        // icon: <ExclamationCircleOutlined />,
-        content: `${record.text} from ${record.username}`,
-        okText: '确认',
-        cancelText: '取消',
-        onOk: () => {
-            console.log(`Deleting.. ${record.text}`)
-        }
-    });
-}
-
 const Comments = ({ match }) => {
     const name = match.params.name
 
@@ -21,14 +8,35 @@ const Comments = ({ match }) => {
 
     const deleteComment = (record) => {
         confirm(record)
-        console.log('Deleting... ' + record )
+        // console.log('Deleting... ' + record)
+    }
+
+    function confirm(record) {
+        Modal.confirm({
+            title: 'Confirm',
+            content: `${record.text} from ${record.username}`,
+            okText: '确认',
+            cancelText: '取消',
+            onOk: () => {
+                // console.log(`Deleting.. ${record.text}`)
+                const removedComments = comments.filter(item => item.index !== record.index)
+                setComments(removedComments)
+            }
+        });
     }
 
     useEffect(() => {
         const fetchComments = async () => {
             const result = await fetch(`/api/articles/${name}`)
             const body = await result.json()
-            setComments(body.comments)
+            const test = body.comments
+            let a = []
+            test.map((item, index) => {
+                item.index = item.username + '-' + index
+                a.push(item)
+            })
+
+            setComments(a)
         }
         fetchComments()
     }, [name])
