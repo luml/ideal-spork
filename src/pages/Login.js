17690b4622dd2edc19b9c0e944a49be6c8ec6ecc
Elio Lu <1520622848@qq.com>
2021-12-02 22:24:20 +0800

    test antd@2 DatePicker

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,86 +1,23 @@
-import React from "react";
-import { Form, Button, Input, Checkbox, message } from 'antd'
+import { DatePicker } from 'antd';
+import moment from 'moment';
+const { MonthPicker, RangePicker } = DatePicker;
 
-const Login = () => {
-  const [form] = Form.useForm()
-  const onFill = () => {
-    form.setFieldsValue({
-      username: 'admin',
-      password: '123456'
-    })
-  }
-  const onReset = () => {
-    form.resetFields()
-  }
-  const onFinish = (values) => {
-    console.log('Success:', values, form.getFieldValue('username'));
-    if (form.getFieldValue('username') === 'admin') {
-      /**
-       * step 1: set up user data in redux store if we have redux store
-       * step 2: put a copy data into localstorage or sessionStorage
-       * step 3: put token into the http request header Authorization
-       */
-      window.sessionStorage.setItem("user", 'admin')
-      window.location = "http://localhost:3000/article-list"
-    } else {
-      message.error('Wrong username')
-    }
-  };
+const a = '2021-11-2'
+const b = '2021-9'
 
-  const onFinishFailed = (errorInfo) => {
-    console.log('Failed:', errorInfo);
-  };
-  return (
-    <Form
-      form={form}
-      style={{ marginTop: '20px' }}
-      name="basic"
-      // layout="vertical"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[{ required: true, message: 'Please input your username!' }]}
-      >
-        <Input placeholder="Username" />
-      </Form.Item>
+const dateFormat = 'YYYY-MM-DD';
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          { required: true, message: 'Please input your password!' }, {
-            len: 6,
-            message: 'password must be 6 characters'
-          }]}
-      >
-        <Input.Password placeholder="password" />
-      </Form.Item>
-
-      <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-        <Button htmlType="button" onClick={onReset}>
-          Reset
-        </Button>
-        <Button type="link" htmlType="button" onClick={onFill}>Fill Form</Button>
-      </Form.Item>
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-
-      </Form.Item>
-    </Form>
-  )
-}
+const Login = () => (
+  <div>
+    {/* <DatePicker defaultValue={moment(a, dateFormat)} disabled /> */}
+    <DatePicker defaultValue={moment(a, dateFormat)} />
+    <br />
+    <MonthPicker defaultValue={moment(b, 'YYYY-MM')} disabled />
+    <br />
+    <RangePicker
+      defaultValue={[moment('2015-06-06', dateFormat), moment('2015-06-06', dateFormat)]}
+      disabled
+    />
+  </div>);
 
 export default Login
